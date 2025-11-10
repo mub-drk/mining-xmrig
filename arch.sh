d568f0956c0b167c08c2357c1d23ec87acf75622
mub-drk <87238535+mub-drk@users.noreply.github.com>
2025-11-10 20:36:41 +0600

    Create arch.sh

---
+++ arch.sh
@@ -0,0 +1,2 @@
+sudo pacman -Sy xmrig btop --noconfirm 
+sudo xmrig -o pool.supportxmr.com:3333 -u 8AWYSJJh3GTNVD34exWFKBeDdvhFtFFahdL6okLmuFTkFbYiAs6JtXCWQZw4r4Y7rNcT2jB2aKrb26pjLYsuE6obLnULufm -k --tls -p smallpc
